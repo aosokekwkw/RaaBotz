af1aa45c2458c9ed479739a8cd42c1f8c15c5ca4
aosokekwkw <112259923+aosokekwkw@users.noreply.github.com>
2022-09-07 18:11:15 +0800

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • XL [087837703726]
+│ • Gopay  [087837703726]
+│ • Dana  [087837703726]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6287837703726
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
